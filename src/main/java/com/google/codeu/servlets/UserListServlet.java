d98eb3b0e2b7fe1f7f9fe84fba78ff00260b6bf4
Jennifer Wang <jennwang@mit.edu>
2019-05-28 16:19:53 -0400

    New UserListServlet code
    uses getUsers function added in Datastore.java

--- src/main/java/com/google/codeu/servlets/UserListServlet.java
+++ src/main/java/com/google/codeu/servlets/UserListServlet.java
@@ -1,7 +1,10 @@
 package com.google.codeu.servlets;
 
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
 import java.io.IOException;
 
+import java.util.Set;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,10 +16,20 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/user-list")
 public class UserListServlet extends HttpServlet {
 
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException {
-
-    response.getOutputStream().println("this will be my user list");
+    response.setContentType("application/json");
+    Set<String> users = datastore.getUsers();
+    Gson gson = new Gson();
+    String json = gson.toJson(users);
+    response.getOutputStream().println(json);
   }
 }
