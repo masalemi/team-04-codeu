b6ef2b9fa136ec8537145c72b15bd958a6e639bf
Chenghan Zhou <chenghanzh@gmail.com>
2019-06-09 22:15:01 -0400

    Format map.js

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -20,15 +20,13 @@ function createMap(){
 }
 
 function createUfoSightingsMap(){
-    fetch('/ufo-data').then(function(response) {
-      return response.json();
-    }).then((ufoSightings) => {
-
+  fetch('/ufo-data').then(function(response) {
+    return response.json();
+  }).then((ufoSightings) => {
     const map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 35.78613674, lng: -119.4491591},
       zoom:3
     });
-
     ufoSightings.forEach((ufoSighting) => {
       new google.maps.Marker({
         position: {lat: ufoSighting.lat, lng: ufoSighting.lng},
