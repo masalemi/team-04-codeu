484fc085505eb89188e3edd6d736723f7cc01b68
Chenghan Zhou <chenghanzh@gmail.com>
2019-06-09 22:02:15 -0400

    Create a map of Ufo in index.html

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -18,3 +18,22 @@ function createMap(){
     trexInfoWindow.open(map, trexMarker);
   });
 }
+
+function createUfoSightingsMap(){
+    fetch('/ufo-data').then(function(response) {
+      return response.json();
+    }).then((ufoSightings) => {
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 35.78613674, lng: -119.4491591},
+      zoom:3
+    });
+
+    ufoSightings.forEach((ufoSighting) => {
+      new google.maps.Marker({
+        position: {lat: ufoSighting.lat, lng: ufoSighting.lng},
+        map: map
+      });
+    });
+  });
+}
