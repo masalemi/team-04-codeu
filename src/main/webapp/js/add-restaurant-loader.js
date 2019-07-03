589d496ed429dfae71c39a84c0bca03a4df37882
masalemi <masalemi29@gmail.com>
2019-07-02 23:26:31 -0400

    Added way to temporarily add restaurants

---
+++ src/main/webapp/js/add-restaurant-loader.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** Sets the page title based on the URL parameter username. */
+function setPageTitle() {
+  document.title = 'Add Restaurant Page';
+}
+
+function fetchBlobstoreUrlAndShowForm() {
+  fetch('/blobstore-upload-url-r')
+    .then((response) => {
+      return response.text();
+    })
+    .then((imageUploadUrl) => {
+      const messageForm = document.getElementById('restaurant-form');
+      messageForm.action = imageUploadUrl;
+      messageForm.classList.remove('hidden');
+    });
+}
+
+/** Fetches data and populates the UI of the page. */
+function addRestaurantUI() {
+  const config = {removePlugins: [ 'ImageUpload' ]};
+  ClassicEditor.create(document.getElementById('restaurant-name'), config );
+  ClassicEditor.create(document.getElementById('restaurant-description'), config );
+  setPageTitle();
+  fetchBlobstoreUrlAndShowForm();
+}
