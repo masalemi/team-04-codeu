4e50bfe46aa603aabd14b5de24c0defa14293143
masalemi <masalemi29@gmail.com>
2019-07-02 23:25:37 -0400

    Added restaurant data to restaurant page

---
+++ src/main/webapp/js/restaurant-page-loader.js
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Get ?restaurant=XYZ parameter value
+const urlParams = new URLSearchParams(window.location.search);
+
+const parameterRestaurant = urlParams.get('restaurantId');
+
+// URL must include ?restaurantId=XYZ parameter. If not, redirect to homepage.
+if (!parameterRestaurant) {
+  window.location.replace('/');
+}
+
+/** Sets the page title based on the URL parameter username. */
+function setPageTitle() {
+  document.title = parameterRestaurant + ' - Restaurant Page';
+}
+
+/** Fetches restaurant description and add to page.*/
+function fetchRestaurantInfo(){
+  const url = '/restaurant?restaurantId=' + parameterRestaurant;
+  fetch(url).then((response) => {
+    return response.text();
+  }).then((restaurant) => {
+  	let restaurant_data = JSON.parse(restaurant);
+  	let description = restaurant_data.description;
+    const descriptionContainer = document.getElementById('description-container');
+    if(description === undefined || description == ''){
+      description = 'This restaurant does not have a description yet.';
+    }
+    descriptionContainer.innerHTML = description;
+  });
+}
+
+
+// /** Fetches messages and add them to the page. */
+// function fetchReviews() {
+//   const url = '/messages?restaurantId=' + parameterRestaurant;
+//   fetch(url)
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((messages) => {
+//         const messagesContainer = document.getElementById('message-container');
+//         console.log(messages.length);
+//         if (messages.length == 0) {
+//           messagesContainer.innerHTML = '<p>This restaurant has no reviews yet.</p>';
+//         } else {
+//           messagesContainer.innerHTML = '';
+//         }
+//         messages.forEach((message) => {
+//           const messageDiv = buildMessageDiv(message);
+//           messagesContainer.appendChild(messageDiv);
+//         });
+//       });
+// }
+
+  // Fetch messages and add them to the page.
+  function fetchReviews(){
+    const url = '/messages?restaurantId=' + parameterRestaurant;
+    fetch(url).then((response) => {
+      return response.json();
+    }).then((messages) => {
+      const messageContainer = document.getElementById('message-container');
+      console.log(messages.length);
+      if(messages.length == 0){
+       messageContainer.innerHTML = '<p>This restaurant has no reviews yet.</p>';
+      }
+      else{
+       messageContainer.innerHTML = '';  
+      }
+      messages.forEach((message) => {  
+       const messageDiv = buildMessageDiv(message);
+       messageContainer.appendChild(messageDiv);
+      });
+    });
+  }
+  
+  // function buildMessageDiv(message){
+  //  const usernameDiv = document.createElement('div');
+  //  usernameDiv.classList.add("left-align");
+  //  usernameDiv.appendChild(document.createTextNode(message.user));
+   
+  //  const timeDiv = document.createElement('div');
+  //  timeDiv.classList.add('right-align');
+  //  timeDiv.appendChild(document.createTextNode(new Date(message.timestamp)));
+   
+  //  const headerDiv = document.createElement('div');
+  //  headerDiv.classList.add('message-header');
+  //  headerDiv.appendChild(usernameDiv);
+  //  headerDiv.appendChild(timeDiv);
+   
+  //  const bodyDiv = document.createElement('div');
+  //  bodyDiv.classList.add('message-body');
+  //  bodyDiv.appendChild(document.createTextNode(message.text));
+   
+  //  const messageDiv = document.createElement('div');
+  //  messageDiv.classList.add("message-div");
+  //  messageDiv.appendChild(headerDiv);
+  //  messageDiv.appendChild(bodyDiv);
+   
+  //  return messageDiv;
+  // }
+
+/**
+ * Builds an element that displays the message.
+ * @param {Message} message
+ * @return {Element}
+ */
+function buildMessageDiv(message) {
+  const headerDiv = document.createElement('div');
+  headerDiv.classList.add('message-header');
+  headerDiv.appendChild(document.createTextNode(
+      message.user + ' - ' + new Date(message.timestamp)));
+
+  const bodyDiv = document.createElement('div');
+  bodyDiv.classList.add('message-body');
+  bodyDiv.innerHTML = message.text;
+
+  const messageDiv = document.createElement('div');
+  messageDiv.classList.add('message-div');
+  messageDiv.appendChild(headerDiv);
+  messageDiv.appendChild(bodyDiv);
+
+  return messageDiv;
+}
+
+function fetchBlobstoreUrlAndShowForm() {
+  fetch('/blobstore-upload-url?restaurantId=' + parameterRestaurant)
+    .then((response) => {
+      return response.text();
+    })
+    .then((imageUploadUrl) => {
+      const messageForm = document.getElementById('review-form');
+      messageForm.action = imageUploadUrl;
+      messageForm.classList.remove('hidden');
+    });
+}
+
+/** Fetches data and populates the UI of the page. */
+function buildRestaurant() {
+  const config = {removePlugins: [ 'ImageUpload' ]};
+  ClassicEditor.create(document.getElementById('message-input'), config);
+  setPageTitle();
+  fetchRestaurantInfo();
+  fetchReviews();
+  fetchBlobstoreUrlAndShowForm();
+}
