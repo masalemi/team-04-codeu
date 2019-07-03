f3bc99fc5a110a93b4a844499a6deb36cd61757d
masalemi <masalemi29@gmail.com>
2019-07-02 23:21:45 -0400

    Updated restaurant with more info

--- src/main/java/com/google/codeu/data/Restaurant.java
+++ src/main/java/com/google/codeu/data/Restaurant.java
@@ -9,11 +9,7 @@ public class Restaurant {
   private String name;
   private String description;
   private ArrayList<String> images;
-  // private String url;		url to restaurant website?
-  // private String menu;		url to menu?
-  // private String maps;		url to google maps/link to navigation page?
 
-  // How should the id of the restaurant be determined? (location, hash of name, maps ID, etc?)
   public Restaurant(UUID id, String name, String description, ArrayList<String> images) {
   	this.id = id;
   	this.name = name;
